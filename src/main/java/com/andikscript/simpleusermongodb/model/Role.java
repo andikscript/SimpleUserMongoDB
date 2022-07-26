7bebe0cd2f9936d5f700f044bb8a7da0916db0f7
andikscript <andikscript@gmail.com>
2022-07-26 16:13:42 +0700

    add role

--- src/main/java/com/andikscript/simpleusermongodb/model/Role.java
+++ src/main/java/com/andikscript/simpleusermongodb/model/Role.java
@@ -5,4 +5,20 @@ public class Role {
     private String id;
 
     private String role;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
 }
