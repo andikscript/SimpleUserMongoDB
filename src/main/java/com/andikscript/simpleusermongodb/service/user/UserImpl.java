9fe337f6799b804e229c9fc3c28b1b18fa004cf2
andikscript <andikscript@gmail.com>
2022-07-26 16:22:51 +0700

    add service create user

--- src/main/java/com/andikscript/simpleusermongodb/service/user/UserImpl.java
+++ src/main/java/com/andikscript/simpleusermongodb/service/user/UserImpl.java
@@ -1,6 +1,8 @@
 package com.andikscript.simpleusermongodb.service.user;
 
 import com.andikscript.simpleusermongodb.model.User;
+import com.andikscript.simpleusermongodb.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -8,9 +10,16 @@ import java.util.Optional;
 
 @Service
 public class UserImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    public UserImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @Override
     public void createUser(User user) {
-        
+        userRepository.save(user);
     }
 
     @Override
