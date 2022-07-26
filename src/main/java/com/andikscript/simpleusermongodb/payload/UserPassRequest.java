bf99480fa5e2b36283f5c3b139e938f69cd5cab1
andikscript <andikscript@gmail.com>
2022-07-26 21:40:39 +0700

    add object for request username and password

---
+++ src/main/java/com/andikscript/simpleusermongodb/payload/UserPassRequest.java
@@ -0,0 +1,24 @@
+package com.andikscript.simpleusermongodb.payload;
+
+public class UserPassRequest {
+
+    private String username;
+
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
