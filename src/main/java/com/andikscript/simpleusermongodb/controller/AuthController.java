e06583419f2475b68fb459b10676df9c8d09f8f0
andikscript <andikscript@gmail.com>
2022-07-26 16:31:49 +0700

    add POST for create user

--- src/main/java/com/andikscript/simpleusermongodb/controller/AuthController.java
+++ src/main/java/com/andikscript/simpleusermongodb/controller/AuthController.java
@@ -1,6 +1,12 @@
 package com.andikscript.simpleusermongodb.controller;
 
+import com.andikscript.simpleusermongodb.message.ResponseMessage;
+import com.andikscript.simpleusermongodb.model.User;
 import com.andikscript.simpleusermongodb.service.user.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,5 +18,19 @@ public class AuthController {
 
     public AuthController(UserService userService) {
         this.userService = userService;
+    }
+
+    @PostMapping(value = "/signup", consumes = "application/json")
+    public ResponseEntity<?> createUser(@RequestBody User user) {
+        if (user.getName() == null || user.getEmail() == null ||
+        user.getUsername() == null || user.getPassword() == null ||
+        user.getRoles().length == 0) {
+            ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body(new ResponseMessage("Error"));
+        }
+
+        userService.createUser(user);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(new ResponseMessage("Successfully create user"));
     }
 }
