86d846b92e5abb3ffc244a41198109778e88d3aa
andikscript <andikscript@gmail.com>
2022-07-26 16:08:40 +0700

    initial commit

---
+++ src/main/java/com/andikscript/simpleusermongodb/SimpleUserMongoDbApplication.java
@@ -0,0 +1,13 @@
+package com.andikscript.simpleusermongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleUserMongoDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleUserMongoDbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/andikscript/simpleusermongodb/SimpleUserMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andikscript.simpleusermongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleUserMongoDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
