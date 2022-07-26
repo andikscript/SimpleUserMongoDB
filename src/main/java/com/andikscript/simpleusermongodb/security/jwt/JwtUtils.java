9d3c801afd80e8c2eaf0b48377f18538c734b5a9
andikscript <andikscript@gmail.com>
2022-07-26 21:38:46 +0700

    add component

--- src/main/java/com/andikscript/simpleusermongodb/security/jwt/JwtUtils.java
+++ src/main/java/com/andikscript/simpleusermongodb/security/jwt/JwtUtils.java
@@ -5,9 +5,11 @@ import io.jsonwebtoken.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
 import java.util.Date;
 
+@Component
 public class JwtUtils {
 
     private static final Logger logger = LoggerFactory.getLogger(JwtUtils.class);
