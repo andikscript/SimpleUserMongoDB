bc3014aef27d51463a6e89d7d44b6aeb82ea56cb
andikscript <andikscript@gmail.com>
2022-07-27 08:50:39 +0700

    fix wrong model

--- src/main/java/com/andikscript/simpleusermongodb/repository/RefreshRepository.java
+++ src/main/java/com/andikscript/simpleusermongodb/repository/RefreshRepository.java
@@ -1,8 +1,12 @@
 package com.andikscript.simpleusermongodb.repository;
 
+import com.andikscript.simpleusermongodb.model.RefreshToken;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface RefreshRepository extends MongoRepository<RefreshRepository, String> {
+public interface RefreshRepository extends MongoRepository<RefreshToken, String> {
+    Optional<RefreshToken> findByToken(String token);
 }
