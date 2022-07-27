826256a47304a4264b8b5dbc3f310eba17a490b9
andikscript <andikscript@gmail.com>
2022-07-27 08:51:06 +0700

    add refresh token

---
+++ src/main/java/com/andikscript/simpleusermongodb/security/refresh/RefreshTokenService.java
@@ -0,0 +1,49 @@
+package com.andikscript.simpleusermongodb.security.refresh;
+
+import com.andikscript.simpleusermongodb.exception.RefreshTokenException;
+import com.andikscript.simpleusermongodb.model.RefreshToken;
+import com.andikscript.simpleusermongodb.repository.RefreshRepository;
+import com.andikscript.simpleusermongodb.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class RefreshTokenService {
+
+    @Value("${SimpleUserMongoDB.app.jwtRefreshExpiration}")
+    private Long refreshTokenDuration;
+
+    @Autowired
+    private RefreshRepository refreshRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public Optional<RefreshToken> findByToken(String token) {
+        return refreshRepository.findByToken(token);
+    }
+
+    public RefreshToken createRefreshToken(String id) {
+        RefreshToken refreshToken = new RefreshToken();
+
+        refreshToken.setIdUser(userRepository.findById(id).get().getId());
+        refreshToken.setToken(UUID.randomUUID().toString());
+        refreshToken.setExpiredDate(Instant.now().plusMillis(refreshTokenDuration));
+        refreshToken = refreshRepository.save(refreshToken);
+        return refreshToken;
+    }
+
+    public RefreshToken verifyExpired(RefreshToken refreshToken) {
+        if (refreshToken.getExpiredDate().compareTo(Instant.now()) < 0) {
+            refreshRepository.delete(refreshToken);
+            throw new RefreshTokenException(refreshToken.getToken(), "Refresh token was expired");
+        }
+
+        return refreshToken;
+    }
+}
