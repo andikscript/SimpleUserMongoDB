f97241fb2bb57a8a972be3a274087304781b2577
andikscript <andikscript@gmail.com>
2022-07-27 13:18:31 +0700

    change package

--- src/main/java/com/andikscript/simpleusermongodb/repository/h2/EmployeeRepository.java
+++ src/main/java/com/andikscript/simpleusermongodb/repository/h2/EmployeeRepository.java
@@ -1,4 +1,4 @@
-package com.andikscript.simpleusermongodb.repository;
+package com.andikscript.simpleusermongodb.repository.h2;
 
 import com.andikscript.simpleusermongodb.model.h2.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
