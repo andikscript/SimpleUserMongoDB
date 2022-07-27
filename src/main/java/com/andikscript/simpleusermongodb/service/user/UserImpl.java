0433d46e496c0a09465a9eae8e75184360501eca
andikscript <andikscript@gmail.com>
2022-07-27 21:35:10 +0700

    add user already exception

--- src/main/java/com/andikscript/simpleusermongodb/service/user/UserImpl.java
+++ src/main/java/com/andikscript/simpleusermongodb/service/user/UserImpl.java
@@ -1,7 +1,13 @@
 package com.andikscript.simpleusermongodb.service.user;
 
+import com.andikscript.simpleusermongodb.handling.FailedValueBody;
+import com.andikscript.simpleusermongodb.handling.UserAlready;
+import com.andikscript.simpleusermongodb.message.ResponseMessage;
 import com.andikscript.simpleusermongodb.model.User;
 import com.andikscript.simpleusermongodb.repository.UserRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -11,12 +17,28 @@ public class UserImpl implements UserService {
 
     private final UserRepository userRepository;
 
-    public UserImpl(UserRepository userRepository) {
+    private final PasswordEncoder passwordEncoder;
+
+    public UserImpl(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
-    public void createUser(User user) {
+    public void createUser(User user) throws FailedValueBody, UserAlready {
+        if (user.getName() == null || user.getEmail() == null ||
+                user.getUsername() == null || user.getPassword() == null ||
+                user.getRoles().length == 0) {
+            throw new FailedValueBody();
+        }
+
+        if (userRepository.findByUsername(user.getUsername()).isPresent() ||
+                userRepository.findByEmail(user.getEmail()).isPresent()) {
+            throw new UserAlready();
+        }
+
+        String password = passwordEncoder.encode(user.getPassword());
+        user.setPassword(password);
         userRepository.save(user);
     }
 
--- src/main/java/com/andikscript/simpleusermongodb/service/user/UserService.java
+++ src/main/java/com/andikscript/simpleusermongodb/service/user/UserService.java
@@ -1,12 +1,14 @@
 package com.andikscript.simpleusermongodb.service.user;
 
+import com.andikscript.simpleusermongodb.handling.FailedValueBody;
+import com.andikscript.simpleusermongodb.handling.UserAlready;
 import com.andikscript.simpleusermongodb.model.User;
 
 import java.util.Optional;
 
 public interface UserService {
 
-    void createUser(User user);
+    void createUser(User user) throws FailedValueBody, UserAlready;
 
     Optional<User> getUserByUsername(String username);
 
