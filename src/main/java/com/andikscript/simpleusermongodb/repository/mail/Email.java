f415f9df7990e185aac9ee5622c702d48e8e70f7
andikscript <andikscript@gmail.com>
2022-08-09 22:59:54 +0700

    add model email

---
+++ src/main/java/com/andikscript/simpleusermongodb/repository/mail/Email.java
@@ -0,0 +1,34 @@
+package com.andikscript.simpleusermongodb.repository.mail;
+
+public class Email {
+
+    private String received;
+
+    private String subject;
+
+    private String message;
+
+    public String getReceived() {
+        return received;
+    }
+
+    public void setReceived(String received) {
+        this.received = received;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
