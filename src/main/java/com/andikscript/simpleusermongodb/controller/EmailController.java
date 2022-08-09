2c9d9aec4c61943c6bf35d35094c92838b005003
andikscript <andikscript@gmail.com>
2022-08-09 23:24:21 +0700

    change http method from post to get

--- src/main/java/com/andikscript/simpleusermongodb/controller/EmailController.java
+++ src/main/java/com/andikscript/simpleusermongodb/controller/EmailController.java
@@ -5,10 +5,7 @@ import com.andikscript.simpleusermongodb.message.ResponseMessage;
 import com.andikscript.simpleusermongodb.service.user.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/api/email/")
@@ -20,7 +17,7 @@ public class EmailController {
         this.userService = userService;
     }
 
-    @PostMapping(value = "/{id}/confirmed")
+    @GetMapping(value = "/{id}/confirmed")
     public ResponseEntity<?> confirmedEmail(@PathVariable(value = "id") String confirmed) throws UserNotRegister {
         userService.findConfirmed(confirmed);
         return ResponseEntity
