ce588ec8b6c52d8fa1bad21edb8d6155c9202aab
andikscript <andikscript@gmail.com>
2022-08-11 08:48:03 +0700

    add random 6 number

--- src/main/java/com/andikscript/simpleusermongodb/util/RandomNumberWord.java
+++ src/main/java/com/andikscript/simpleusermongodb/util/RandomNumberWord.java
@@ -16,4 +16,13 @@ public class RandomNumberWord {
             sb.append(chars.charAt(rnd.nextInt(chars.length())));
         return sb.toString();
     }
+    public String randomNumber() {
+        String chars = "0123456789";
+        Random rnd = new Random();
+        StringBuilder sb = new StringBuilder(6);
+        for (int i = 0; i < 6; i++)
+            sb.append(chars.charAt(rnd.nextInt(chars.length())));
+        return sb.toString();
+    }
+
 }
