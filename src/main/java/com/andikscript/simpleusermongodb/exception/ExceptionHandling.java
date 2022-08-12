82159beff9d6eb8484a1b0fbd6be61592d55eede
andikscript <andikscript@gmail.com>
2022-08-12 20:37:14 +0700

    add exception if input not valid

--- src/main/java/com/andikscript/simpleusermongodb/exception/ExceptionHandling.java
+++ src/main/java/com/andikscript/simpleusermongodb/exception/ExceptionHandling.java
@@ -3,12 +3,21 @@ package com.andikscript.simpleusermongodb.exception;
 
 import com.andikscript.simpleusermongodb.handling.*;
 import com.andikscript.simpleusermongodb.message.ResponseMessage;
+import com.andikscript.simpleusermongodb.model.error.Error;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestControllerAdvice
 public class ExceptionHandling extends ResponseEntityExceptionHandler {
@@ -52,5 +61,23 @@ public class ExceptionHandling extends ResponseEntityExceptionHandler {
         return ResponseEntity
                 .status(HttpStatus.EXPECTATION_FAILED)
                 .body(new ResponseMessage("User not verify"));
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        List<String> getError = new ArrayList<>();
+
+        for (FieldError err : ex.getFieldErrors()) {
+            getError.add(err.getDefaultMessage());
+        }
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(new Error(
+                        HttpStatus.BAD_REQUEST,
+                        LocalDateTime.now(),
+                        "Validate Error",
+                        getError
+                ));
     }
 }
