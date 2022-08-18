721567663591d159c6ac2d1237dd2f9f8850740c
andikscript <andikscript@gmail.com>
2022-08-18 19:16:17 +0700

    add update employee

--- src/main/java/com/andikscript/simpleusermongodb/service/employee/EmployeImpl.java
+++ src/main/java/com/andikscript/simpleusermongodb/service/employee/EmployeImpl.java
@@ -1,10 +1,13 @@
 package com.andikscript.simpleusermongodb.service.employee;
 
+import com.andikscript.simpleusermongodb.handling.UserNotFound;
 import com.andikscript.simpleusermongodb.model.postgresql.Employee;
 import com.andikscript.simpleusermongodb.repository.h2.EmployeeRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class EmployeImpl implements EmployeeService {
@@ -23,5 +26,17 @@ public class EmployeImpl implements EmployeeService {
     @Override
     public List<Employee> getAllEmployee() {
         return employeeRepository.findAll();
+    }
+
+    @Override
+    public void updateEmployee(Employee employee, UUID id) throws UserNotFound {
+        Optional<Employee> getEmployee1 = employeeRepository.findById(id);
+
+        if (!getEmployee1.isPresent()) {
+            throw new UserNotFound();
+        }
+
+        employee.setId(id);
+        employeeRepository.save(employee);
     }
 }
--- src/main/java/com/andikscript/simpleusermongodb/service/employee/EmployeeService.java
+++ src/main/java/com/andikscript/simpleusermongodb/service/employee/EmployeeService.java
@@ -1,12 +1,16 @@
 package com.andikscript.simpleusermongodb.service.employee;
 
+import com.andikscript.simpleusermongodb.handling.UserNotFound;
 import com.andikscript.simpleusermongodb.model.postgresql.Employee;
 
 import java.util.List;
+import java.util.UUID;
 
 public interface EmployeeService {
 
     void createEmployee(Employee employee);
 
     List<Employee> getAllEmployee();
+
+    void updateEmployee(Employee employee, UUID id) throws UserNotFound;
 }
