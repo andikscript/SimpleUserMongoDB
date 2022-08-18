1a3ee80f7b82fe062148611db677dee63c809ca2
andikscript <andikscript@gmail.com>
2022-08-18 20:03:31 +0700

    custom message if not valid token

--- src/main/java/com/andikscript/simpleusermongodb/security/jwt/AuthEntryPointJwt.java
+++ src/main/java/com/andikscript/simpleusermongodb/security/jwt/AuthEntryPointJwt.java
@@ -1,5 +1,9 @@
 package com.andikscript.simpleusermongodb.security.jwt;
 
+import com.andikscript.simpleusermongodb.model.error.Error;
+import org.json.JSONObject;
+import org.springframework.boot.json.GsonJsonParser;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
@@ -8,6 +12,8 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.Collections;
 
 @Component
 public class AuthEntryPointJwt implements AuthenticationEntryPoint {
@@ -15,6 +21,15 @@ public class AuthEntryPointJwt implements AuthenticationEntryPoint {
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response,
                          AuthenticationException authException) throws IOException, ServletException {
-        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Error : Unauthorized");
+        response.setContentType("application/json");
+        response.setStatus(401);
+        response.setCharacterEncoding("UTF-8");
+        response.getWriter().write(String.valueOf(new JSONObject(new Error(
+                HttpStatus.UNAUTHORIZED,
+                LocalDateTime.now(),
+                "Error : Unauthorized",
+                Collections.singletonList("Please enter valid Token")
+        ))));
+//        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Error : Unauthorized");
     }
 }
