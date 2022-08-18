5ff921e48d8cb6a595caf8e96c5ce9389fcea7b2
andikscript <andikscript@gmail.com>
2022-08-18 20:18:16 +0700

    add limit access

--- src/main/java/com/andikscript/simpleusermongodb/service/employee/EmployeeService.java
+++ src/main/java/com/andikscript/simpleusermongodb/service/employee/EmployeeService.java
@@ -2,17 +2,22 @@ package com.andikscript.simpleusermongodb.service.employee;
 
 import com.andikscript.simpleusermongodb.handling.UserNotFound;
 import com.andikscript.simpleusermongodb.model.postgresql.Employee;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 import java.util.List;
 import java.util.UUID;
 
 public interface EmployeeService {
 
+    @PreAuthorize("hasRole('ADMIN') or hasRole('ROOT')")
     void createEmployee(Employee employee);
 
+    @PreAuthorize("hasRole('USER') or hasRole('ADMIN') or hasRole('ROOT')")
     List<Employee> getAllEmployee();
 
+    @PreAuthorize("hasRole('ROOT')")
     void updateEmployee(Employee employee, UUID id) throws UserNotFound;
 
+    @PreAuthorize("hasRole('ROOT')")
     void deleteEmployee(UUID id) throws UserNotFound;
 }
